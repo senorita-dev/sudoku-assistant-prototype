010b6b3cde25edb3800286216cce4689e18c7dfe
Ren Saito <ren.saito.dev@gmail.com>
2025-07-02 22:11:36 +1200

    feat: add style to relevant candidates for reduce step type

--- components.py
+++ components.py
@@ -61,11 +61,6 @@ def sudoku_cell(
         style = style | {
             "backgroundColor": "lightgreen" if y == new_y and x == new_x else None
         }
-    if view_board_details and step["type"] == "reduce":
-        positions = step["positions"]
-        style = style | {
-            "backgroundColor": "lightblue" if [y, x] in positions else None
-        }
     return html.Td(
         (
             ""
@@ -138,30 +133,20 @@ def candidates_cell(
             style=divStyle,
         )
     removed_digits = step["removed_digits"]
-    positions = step["candidates_removed_positions"]
-    return html.Div(
-        [
-            html.Span(
-                digit if digit in digits else "",
-                style=spanStyle
-                | {
-                    "backgroundColor": (
-                        "red"
-                        if digit in digits
-                        and digit in removed_digits
-                        and [y, x] in positions
-                        else None
-                    ),
-                    "color": (
-                        "white"
-                        if digit in digits
-                        and digit in removed_digits
-                        and [y, x] in positions
-                        else None
-                    ),
-                },
-            )
-            for digit in range(1, 10)
-        ],
-        style=divStyle,
-    )
+    relevant_positions = step["positions"]
+    removed_positions = step["candidates_removed_positions"]
+    spans = []
+    for digit in range(1, 10):
+        style = {}
+        if digit in digits and digit in removed_digits:
+            if [y, x] in removed_positions:
+                style["backgroundColor"] = "red"
+                style["color"] = "white"
+            elif [y, x] in relevant_positions:
+                style["backgroundColor"] = "lightblue"
+                style["color"] = "white"
+                style["fontWeight"] = "bold"
+        spans.append(
+            html.Span(digit if digit in digits else "", style=spanStyle | style)
+        )
+    return html.Div(spans, style=divStyle)
